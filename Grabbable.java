00ffcca7aa6fdc4b2c04efc9357d2c40d92cef74
alexandrahuayta <ahuaytaalata@smith.edu>
2025-12-10 19:23:52 -0500

    I edited Grabbable.java

--- Grabbable.java
+++ Grabbable.java
@@ -1,11 +1,42 @@
-public class Grabbable { 
+public class Grabbable {
+
     protected String name;
 
-    public Grabbable(String name){
+    public Grabbable(String name) {
         this.name = name;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
+    }
+
+    // ------------------------- Food -------------------------
+    public static class Food extends Grabbable {
+
+        private int energyGain;
+
+        public Food(String name, int energyGain) {
+            super(name);
+            this.energyGain = energyGain;
+        }
+
+        public int getEnergyGain() {
+            return energyGain;
+        }
+    }
+
+    // ------------------------- Water -------------------------
+    public static class Water extends Grabbable {
+
+        private int energyGain;
+
+        public Water(String name, int energyGain) {
+            super(name);
+            this.energyGain = energyGain;
+        }
+
+        public int getEnergyGain() {
+            return energyGain;
+        }
     }
 }
