3dc9c7891b18a1dcb0c2fa57fa92d0dad5d74085
Diane Tuyizere <dtuyizere@smith.edu>
2025-12-11 21:08:45 -0500

    Added javadocs for grabbable

--- Grabbable.java
+++ Grabbable.java
@@ -1,11 +1,22 @@
+/**
+ * this is the footprint of the grabable items in the game
+ * @author Greta Wang, Diane Tuyizere, Alexa Huayta
+ * @version 12/11/2025
+ */
 public class Grabbable {
 
     protected String name;
-
+/**
+ * the constructor
+ * @param name
+ */
     public Grabbable(String name) {
         this.name = name;
     }
-
+/**
+ * the getter method to help accessing the name
+ * @param name
+ */
     public String getName() {
         return this.name;
     }
